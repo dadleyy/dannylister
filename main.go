624edd83d4d2b6dec68011636ea5f70321b2fc80
Danny Hadley <dadleyy@gmail.com>
2016-08-01 22:41:07 -0400

    initial pass at collecting a tree structure of files/directories

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import "os"
+import "fmt"
+import "flag"
+import "path/filepath"
+
+type optionsT struct {
+	Help bool
+	Recursive bool
+	WorkingDir string
+	Output string
+}
+
+type treeNode struct {
+	Name string
+	FullPath string
+	Children []treeNode
+	Link string
+}
+
+func (t *treeNode) Collect(recurse bool) error {
+	info, err := os.Stat(t.FullPath)
+
+	if err != nil {
+		return err
+	}
+
+	t.Name = info.Name()
+
+	if info.IsDir() != true {
+		return nil
+	}
+
+	file, err := os.Open(t.FullPath)
+
+	if err != nil {
+		return err
+	}
+
+	children, err := file.Readdir(0)
+
+	if err != nil {
+		return err
+	}
+
+	// now that we've collected all of the FileInfo objects of items in our directory,
+	// we can loop over each creating new nodes and adding them into our children slice
+	for _, info := range children {
+		// build the full path to this child from our path and their name
+		full := filepath.Join(t.FullPath, info.Name())
+
+		// create the instance of the tree node
+		child := treeNode{info.Name(), full, make([]treeNode, 0), ""}
+
+		// find out if this is a symbolic link
+		if mode := info.Mode(); (mode & os.ModeSymlink) != 0 {
+			dest, err := os.Readlink(full)
+
+			// if we received an error here something strange happened; the os told us this
+			// file had a symbolic link mode but was unable to resolve it to the destination
+			if err != nil {
+				return err
+			}
+
+			child.Link = dest
+
+			t.Children = append(t.Children, child)
+			continue
+		}
+
+		// if we were told to recurse, have the child collect all it's nodes as well
+		if recurse == true {
+			err = child.Collect(true)
+			if err != nil {
+				return err
+			}
+		}
+
+		// add this child to our list and move on
+		t.Children = append(t.Children, child)
+	}
+
+	return nil
+}
+
+func main() {
+	var options optionsT
+	flag.BoolVar(&options.Help, "help", false, "display this help text")
+	flag.BoolVar(&options.Recursive, "recursive", false, "when set, list files recursively. default: false")
+	flag.StringVar(&options.WorkingDir, "path", "./", "specifies the working directory for the file scan")
+	flag.StringVar(&options.Output, "output", "text", "specifies the type of format to output to stdout. options are: \"json\", \"yaml\" or \"text\". default is \"text\"")
+
+	flag.Parse()
+
+	if options.Help == true {
+		flag.PrintDefaults()
+		return
+	}
+
+	fmt.Printf("help[%t] recursive[%t] path[%s] format[%s]\n", options.Help, options.Recursive, options.WorkingDir, options.Output)
+
+	info, err := os.Stat(options.WorkingDir)
+
+	if err != nil {
+		fmt.Printf("Error parsing working directory: %s\n", err.Error())
+		return
+	}
+
+	if info.IsDir() != true {
+		fmt.Printf("Path provided is not a directory\n")
+		return
+	}
+
+	tree := treeNode{options.WorkingDir, options.WorkingDir, make([]treeNode, 0), ""}
+
+	err = tree.Collect(options.Recursive)
+
+	fmt.Println(tree)
+}
